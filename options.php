172ffefb33d5a9002f0a579c57c38f9bf3883b9e
Oleh <oleh.leskiv.4@gmail.com>
2015-07-03 21:32:55 +0300

    Review deleted

--- options.php
+++ options.php
@@ -1,6 +1,6 @@
 <?php
 	$availableLanguages = array("uk", "ru", "pl","gb","sr");
-	$availablePages = array("home", "about-montenegro", "map", "services-pricing", "contacts", "review");
+	$availablePages = array("home", "about-montenegro", "map", "services-pricing", "contacts");
 	$pageParseTags = array("title", "description", "content", "keywords");
 	$outputRemainedAfterParse = false;
 ?>
